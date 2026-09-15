90f0210d614cd92aca915c4eea0be837eabf2a10
EmptyTeacup-afk <rbeaumont744@gmail.com>
2026-09-15 15:04:31 +0200

    WeekOne

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+
+  </p>`;
+}
+
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
